f680f12b161d4578377af984e0f7cf5820a752e4
Disha Kamble <120121137+dishak24@users.noreply.github.com>
2025-03-26 13:11:45 +0530

    Manager Layer: Business Logic

---
+++ ManagerLayer/Interfaces/IUserManager.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer.Models;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface IUserManager
+    {
+        public UserEntity Register(RegisterModel model);
+
+        public bool CheckEmailDuplicate(string email);
+    }
+}
---
+++ ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,30 @@
+﻿using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepo userRepo;
+
+        public UserManager(IUserRepo userRepo)
+        {
+            this.userRepo = userRepo;
+        }
+
+        public UserEntity Register(RegisterModel model)
+        {
+            return userRepo.Register(model);
+        }
+
+        public bool CheckEmailDuplicate(string email)
+        {
+            return userRepo.CheckEmailDuplicate(email);
+        }
+    }
+}
