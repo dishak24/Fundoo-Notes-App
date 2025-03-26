413483c2c1767fcccd08147ea261ff4773988bd8
Disha Kamble <120121137+dishak24@users.noreply.github.com>
2025-03-26 13:18:23 +0530

    Register with email checking & Password Encrypt

---
+++ RepositoryLayer/Interfaces/IUserRepo.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer.Models;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface IUserRepo
+    {
+        public UserEntity Register(RegisterModel model);
+
+        //for duplicate email checking
+        public bool CheckEmailDuplicate(string email);
+    }
+}
---
+++ RepositoryLayer/Services/UserRepo.cs
@@ -0,0 +1,60 @@
+﻿using CommonLayer.Models;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class UserRepo : IUserRepo
+    {
+        private readonly FundooDBContext context;
+        public UserRepo(FundooDBContext context)
+        {
+            this.context = context;
+        }
+
+        public UserEntity Register(RegisterModel model)
+        {
+            UserEntity user = new UserEntity();
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.DOB = model.DOB;
+            user.Gender = model.Gender;
+            user.Email = model.Email;
+            user.Password = EncodePasswordToBase64(model.Password);
+
+            this.context.Users.Add(user);
+            context.SaveChanges();
+            return user;
+        }
+
+        public static string EncodePasswordToBase64(string password)
+        {
+            try
+            {
+                byte[] encryption_Data = new byte[password.Length];
+                encryption_Data = System.Text.Encoding.UTF8.GetBytes(password);
+                string encoded_Data = Convert.ToBase64String(encryption_Data);
+                return encoded_Data;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Error in base64 encode " +e.Message);
+            }
+        }
+
+        public bool CheckEmailDuplicate(string email)
+        {
+            var result = this.context.Users.FirstOrDefault(x => x.Email == email);
+            if (result == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
