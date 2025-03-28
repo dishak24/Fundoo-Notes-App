95c355f8689d25f5ecc981314a8a65eedae1e0df
Disha <dishakamble24@gmail.com>
2025-03-28 16:19:14 +0530

    Create Note Api added

---
+++ FundooNotesApp/Controllers/NotesController.cs
@@ -0,0 +1,61 @@
+﻿using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+//using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FundooNotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INotesManager notesManager;
+        public NotesController(INotesManager notesManager)
+        {
+            this.notesManager = notesManager;
+        }
+
+        //API for Creating/adding new Note
+        [HttpPost]
+        [Route("AddNote")]
+        public IActionResult CreateNote(NotesModel notesModel)
+        {
+            try
+            {
+               int userId = int.Parse(User.FindFirst("UserId").Value);
+
+                var note = notesManager.CreateNote(userId, notesModel);
+                if (note != null)
+                {
+                    return Ok(new ResponseModel<NotesEntity>
+                    {
+                        Success = true,
+                        Message = "Done, Note Added successfully !",
+                        Data = note
+
+                    });
+                }
+                else
+                {
+                    return BadRequest(new ResponseModel<string>
+                    {
+                        Success = true,
+                        Message = "Adding Notes Failed !!!!!"
+
+                    });
+                }
+
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        
+    }
+}
