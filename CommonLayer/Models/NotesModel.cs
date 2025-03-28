28fadcc270575b193dffdaf444cd5b6bae05bedc
Disha <dishakamble24@gmail.com>
2025-03-28 16:02:15 +0530

    Notes Model Added

---
+++ CommonLayer/Models/NotesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CommonLayer.Models
+{
+    public class NotesModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
